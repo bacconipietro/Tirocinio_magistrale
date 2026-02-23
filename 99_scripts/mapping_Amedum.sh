e67d65e7f7a139e74d3894661cb44dd2ab0e12fa
Pietro Bacconi <bacconi.pietro@gmail.com>
2026-02-23 10:33:33 +0100

    Fix command syntax in mapping_Amedum.sh

--- 99_scripts/mapping_Amedum.sh
+++ 99_scripts/mapping_Amedum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Mapping short reads on assembly. Mutate SAM into BAM
-minimap2 -ax sr --MD -t 6 node_18_runall.fasta Amedum_1_paired.fastq  Amedum_2_paired.fastq > Amedum_raw_sr.sam
+minimap2 -ax sr --MD -t 6 node_18_runall.fasta Amedum_1_paired.fastq Amedum_2_paired.fastq > Amedum_raw_sr.sam
 samtools view -Sb Amedum_raw_sr.sam > Amedum_raw_sr.bam
 rm Amedum_raw_sr.sam
 samtools sort -@6 -o Amedum_raw_sr_sorted.bam Amedum_raw_sr.bam

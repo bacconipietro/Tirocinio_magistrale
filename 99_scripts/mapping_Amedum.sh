f31fd031e681fe05fcba42f3b1ec98d77aa5eeec
Pietro Bacconi <bacconi.pietro@gmail.com>
2026-02-23 10:29:59 +0100

    Change indexing to sorted BAM file

--- 99_scripts/mapping_Amedum.sh
+++ 99_scripts/mapping_Amedum.sh
@@ -5,5 +5,5 @@ minimap2 -ax sr --MD -t 6 node_18_runall.fasta Amedum_1_paired.fastq  Amedum_2_p
 samtools view -Sb Amedum_raw_sr.sam > Amedum_raw_sr.bam
 rm Amedum_raw_sr.sam
 samtools sort -@6 -o Amedum_raw_sr_sorted.bam Amedum_raw_sr.bam
-samtools index Amedum_raw_sr.bam
+samtools index Amedum_raw_sr_sorted.bam
 rm Amedum_raw_sr.bam

9bd83aad19f4e0ff8a999d18b74c6c82ae6b7b12
bacconi.pietro <bacconi.pietro@gmail.com>
2026-02-23 09:38:45 +0100

    ...

--- 99_scripts/mapping_Amedum.sh
+++ 99_scripts/mapping_Amedum.sh
@@ -2,7 +2,7 @@
 
 # Mapping short reads on assembly. Mutate SAM into BAM
 minimap2 -ax sr --MD -t 6 node_18_runall.fasta Amedum_1_paired.fastq  Amedum_2_paired.fastq > Amedum_raw_sr.sam
-samtools view -Sb Amedum_raw_sr.sam > Amedum_raw.bam
+samtools view -Sb Amedum_raw_sr.sam > Amedum_raw_sr.bam
 rm Amedum_raw_sr.sam
 samtools sort -@6 -o Amedum_raw_sr_sorted.bam Amedum_raw_sr.bam
 samtools index Amedum_raw_sr.bam

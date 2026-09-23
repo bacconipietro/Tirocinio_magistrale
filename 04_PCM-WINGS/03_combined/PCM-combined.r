291f9e2fc0687375bcd6baf3cd4cf428b7dd0824
bacconi.pietro <bacconi.pietro@gmail.com>
2026-09-23 11:07:55 +0200

    adding new ocm folder

---
+++ 04_PCM-WINGS/03_combined/PCM-combined.r
@@ -0,0 +1,19 @@
+setwd("/home/STUDENTI/pietro.bacconi/Tirocinio_magistrale/04_PCM-wings/03_combined")
+library(phytools)
+Loading required package: ape
+Loading required package: maps
+librWarning messages:
+1: package ‘ape’ was built under R version 4.4.3 
+2: package ‘maps’ was built under R version 4.4.3 
+library(geiger)
+tree <- read.tree("aa_iln2_noHPD.tre")
+fem <- read.csv("female_wings_recoded_bin.csv", row.names = 1)  # where macropterous = 1 and reduced(brachypterous+apterous)  = 2
+male <- read.csv("male_wings_recoded_bin.csv", row.names = 1)   # same binary pattern
+x <- setNames(as.character(fem$wings), rownames(fem))
+y <- setNames(as.character(male$wings), rownames(male))
+length(x)
+[1] 189
+length(y)
+[1] 189
+fit_dep <- fitPagel(tree, x, y)
+print(fit_dep)
